f33f5fe1dc11941be5c2cd78515027102befeae2
DiaSird <78157340+DiaSird@users.noreply.github.com>
2025-11-27 00:21:27 +0900

    Fix library path in build script to use x64 directory for SDL2

--- code/desktop/build.rs
+++ code/desktop/build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let crate_root = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let lib_path = crate_root.join("../../SDL2-2.32.10/lib/x86");
+    let lib_path = crate_root.join("../../SDL2-2.32.10/lib/x64");
 
     println!("cargo:rustc-link-search={}", lib_path.display());
     // https://doc.rust-lang.org/cargo/reference/build-scripts.html#rustc-link-lib
